985bd7cef064ed7576fbf86c250db1e6b923a603
Brandonespinoza23 <64062leo@universidaddeleon.edu.mx>
2025-09-25 19:39:22 -0600

    Funcionalidad de raíz cuadrada implementada

--- clases/avanzadas.py
+++ clases/avanzadas.py
@@ -2,12 +2,17 @@ import math
 
 class Avanzadas:
     def __init__(self):
-        self.num1 = 0
-        self.num2 = 0
-
+        self.base = 0
+        self.exponente = 0
+        
     def leerNumeros(self):
-        self.num1 = int(input("Número base: "))
-        self.num2 = int(input("Exponente: "))
+        self.base = float(input("Ingresa la base: "))
+        self.exponente = float(input("Ingresa el exponente: "))
 
     def elevarPotencia(self):
-        return math.pow(self.num1, self.num2)
+        return math.pow(self.base, self.exponente)
+    
+    def raizCuadrada(self):
+        if self.base < 0:
+            return "Error: No se puede calcular la raíz cuadrada de un número negativo."
+        return math.sqrt(self.base)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from clases.avanzadas import avanzadas
+from clases.avanzadas import Avanzadas
 
 if __name__ == "__main__":
-    calc = avanzadas()
+    calc = Avanzadas()
     calc.leerNumeros()
-    print("Resultado potencia:", calc.elevarPotencia())
+    print("Resultado de la potencia:", calc.elevarPotencia())
+    print("Resultado raíz cuadrada:", calc.raizCuadrada())
